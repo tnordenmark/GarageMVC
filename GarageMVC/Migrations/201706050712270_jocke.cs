4a33a3bb4a8f58a8c5a6b1c27b2e941d089b5c45
Jocke <jocke@ny-flik.se>
2017-06-05 09:36:52 +0200

    search fix

---
+++ GarageMVC/Migrations/201706050712270_jocke.cs
@@ -0,0 +1,31 @@
+namespace GarageMVC.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class jocke : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Vehicles",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        ParkingDate = c.DateTime(nullable: false),
+                        ParkingPlace = c.Int(nullable: false),
+                        ParkingPrice = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        RegNumber = c.String(nullable: false, maxLength: 6),
+                        Type = c.Int(nullable: false),
+                        Owner = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Vehicles");
+        }
+    }
+}
